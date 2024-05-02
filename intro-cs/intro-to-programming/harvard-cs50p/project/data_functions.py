7e08bb8f65848d9d40db752733161a10c59d107f
temanmd <temacxz@gmail.com>
2024-05-02 08:41:31 +0500

    Move data functions to sep arate file

---
+++ intro-cs/intro-to-programming/harvard-cs50p/project/data_functions.py
@@ -0,0 +1,83 @@
+from errors import AccountIsFrozenError, AccountNotFoundError, OutOfMoneyError
+
+
+def get_ids(bank):
+    return sorted(map(lambda account: account["id"], bank["accounts"]))
+
+
+def get_accounts(bank):
+    return bank["accounts"]
+
+
+def add_account(info, bank):
+    ids = get_ids(bank)
+    new_id = ids[-1] + 1 if ids else 1
+    new_account = {"id": new_id, "name": info["name"], "balance": 0, "status": "active"}
+    bank["accounts"].append(new_account)
+    return new_account
+
+
+def get_account(id, bank):
+    ids = get_ids(bank)
+    if id in ids:
+        return list(filter(lambda account: account["id"] == id, bank["accounts"]))[0]
+    else:
+        raise AccountNotFoundError(id)
+
+
+def freeze_account(id, bank):
+    account = get_account(id, bank=bank)
+    if account["status"] != "frozen":
+        index = bank["accounts"].index(account)
+        account["status"] = "frozen"
+        bank["accounts"][index] = account
+    return account
+
+
+def unfreeze_account(id, bank):
+    account = get_account(id, bank=bank)
+    if account["status"] == "frozen":
+        index = bank["accounts"].index(account)
+        account["status"] = "active"
+        bank["accounts"][index] = account
+    return account
+
+
+def change_account_name(id, new_name, bank):
+    account = get_account(id, bank=bank)
+    index = bank["accounts"].index(account)
+    account["name"] = new_name
+    bank["accounts"][index] = account
+    return account
+
+
+def deposit(id, amount, bank):
+    account = get_account(id, bank=bank)
+    if account["status"] == "frozen":
+        raise AccountIsFrozenError(id)
+    index = bank["accounts"].index(account)
+    account["balance"] += amount
+    bank["accounts"][index] = account
+    return account
+
+
+def withdraw(id, amount, bank):
+    account = get_account(id, bank=bank)
+    if account["balance"] < amount:
+        raise OutOfMoneyError(id, account["balance"])
+    if account["status"] == "frozen":
+        raise AccountIsFrozenError(1)
+    index = bank["accounts"].index(account)
+    account["balance"] -= amount
+    bank["accounts"][index] = account
+    return account
+
+
+def init(bank_name):
+    bank = {
+        "name": bank_name.strip(),
+        "accounts": [],
+    }
+    account = add_account({"name": "Boss"}, bank=bank)
+    deposit(account["id"], amount=80_000_000_000, bank=bank)
+    return bank
--- intro-cs/intro-to-programming/harvard-cs50p/project/project.py
+++ intro-cs/intro-to-programming/harvard-cs50p/project/project.py
@@ -2,91 +2,20 @@ import re
 import sys
 from errors import AccountIsFrozenError, AccountNotFoundError, OutOfMoneyError
 from terminal_colors import TColors
+from data_functions import (
+    get_accounts,
+    get_account,
+    add_account,
+    freeze_account,
+    unfreeze_account,
+    change_account_name,
+    deposit,
+    withdraw,
+    init,
+)
 
 
 GOODBYE_TEXT = "Good bye!"
-
-
-def get_ids(bank):
-    return sorted(map(lambda account: account["id"], bank["accounts"]))
-
-
-def get_accounts(bank):
-    return bank["accounts"]
-
-
-def add_account(info, bank):
-    ids = get_ids(bank)
-    new_id = ids[-1] + 1 if ids else 1
-    new_account = {"id": new_id, "name": info["name"], "balance": 0, "status": "active"}
-    bank["accounts"].append(new_account)
-    return new_account
-
-
-def get_account(id, bank):
-    ids = get_ids(bank)
-    if id in ids:
-        return list(filter(lambda account: account["id"] == id, bank["accounts"]))[0]
-    else:
-        raise AccountNotFoundError(id)
-
-
-def freeze_account(id, bank):
-    account = get_account(id, bank=bank)
-    if account["status"] != "frozen":
-        index = bank["accounts"].index(account)
-        account["status"] = "frozen"
-        bank["accounts"][index] = account
-    return account
-
-
-def unfreeze_account(id, bank):
-    account = get_account(id, bank=bank)
-    if account["status"] == "frozen":
-        index = bank["accounts"].index(account)
-        account["status"] = "active"
-        bank["accounts"][index] = account
-    return account
-
-
-def change_account_name(id, new_name, bank):
-    account = get_account(id, bank=bank)
-    index = bank["accounts"].index(account)
-    account["name"] = new_name
-    bank["accounts"][index] = account
-    return account
-
-
-def deposit(id, amount, bank):
-    account = get_account(id, bank=bank)
-    if account["status"] == "frozen":
-        raise AccountIsFrozenError(id)
-    index = bank["accounts"].index(account)
-    account["balance"] += amount
-    bank["accounts"][index] = account
-    return account
-
-
-def withdraw(id, amount, bank):
-    account = get_account(id, bank=bank)
-    if account["balance"] < amount:
-        raise OutOfMoneyError(id, account["balance"])
-    if account["status"] == "frozen":
-        raise AccountIsFrozenError(1)
-    index = bank["accounts"].index(account)
-    account["balance"] -= amount
-    bank["accounts"][index] = account
-    return account
-
-
-def init(bank_name):
-    bank = {
-        "name": bank_name.strip(),
-        "accounts": [],
-    }
-    account = add_account({"name": "Boss"}, bank=bank)
-    deposit(account["id"], amount=80_000_000_000, bank=bank)
-    return bank
 
 
 def welcome_info(bank_name):
@@ -104,14 +33,6 @@ def welcome_info(bank_name):
         f"    {TColors.BOLD}help{TColors.ENDC} (show this message again)\n"
         f"    {TColors.BOLD}exit{TColors.ENDC} (exit the program)"
     )
-
-
-def render_accounts(accounts):
-    print(accounts)
-
-
-def render_account(id, bank):
-    print(get_account(id, bank=bank))
 
 
 def validate_command(command):
@@ -138,9 +59,9 @@ def validate_command(command):
 
 def process_show_action(params, bank):
     if params[0] == "all":
-        render_accounts(bank["accounts"])
+        print(get_accounts(bank=bank))
     else:
-        render_account(int(params[0]), bank)
+        print(get_account(int(params[0]), bank=bank))
 
 
 def process_add_action(params, bank):
