daad684a0de5795a135c8bae303bc4b7f0be6348
temanmd <temacxz@gmail.com>
2024-06-30 13:30:52 +0500

    Add mit-6_0001 problem-set-1 A solution

---
+++ intro-cs/intro-to-computer-science/mit-6_0001/problem-set-1/solution_a.py
@@ -0,0 +1,31 @@
+def calc_number_of_months(annual_salary, portion_saved, total_cost):
+    portion_down_payment = 0.25
+    current_savings = 0
+    investment_annual_return = 0.04
+    monthly_salary = annual_salary / 12
+    down_payment = total_cost * portion_down_payment
+    number_of_months = 0
+    monthly_salary_portion_saved = monthly_salary * portion_saved
+
+    while current_savings < down_payment:
+        additional_monthly_return = current_savings * investment_annual_return / 12
+        current_savings += monthly_salary_portion_saved
+        current_savings += additional_monthly_return
+        number_of_months += 1
+
+    return number_of_months
+
+
+def main():
+    annual_salary = int(input("Enter your annual salary: ").strip())
+    portion_saved = float(
+        input("Enter the percent of your salary to save, as a decimal: ").strip()
+    )
+    total_cost = int(input("Enter the cost of your dream home: ").strip())
+
+    result = calc_number_of_months(annual_salary, portion_saved, total_cost)
+    print(f"Number of months: {result}")
+
+
+if __name__ == "__main__":
+    main()
