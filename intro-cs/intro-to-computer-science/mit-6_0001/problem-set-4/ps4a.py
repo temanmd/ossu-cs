77b3aa1d9246aa1155e6ee4710fb4166037a36f2
temanmd <temacxz@gmail.com>
2024-09-30 10:32:09 +0500

    Add mit-6_0001 problem-set-4 A solution

--- intro-cs/intro-to-computer-science/mit-6_0001/problem-set-4/ps4a.py
+++ intro-cs/intro-to-computer-science/mit-6_0001/problem-set-4/ps4a.py
@@ -23,8 +23,16 @@ def get_permutations(sequence):
     Note: depending on your implementation, you may return the permutations in
     a different order than what is listed here.
     """
-
-    pass  # delete this line and replace with your code here
+    if len(sequence) == 1:
+        return set(sequence)
+    if len(sequence) == 2:
+        return set({sequence, sequence[1] + sequence[0]})
+    result = set()
+    for index in range(len(sequence)):
+        chars_range = sequence[:index] + sequence[index + 1 :]
+        for permutation in get_permutations(chars_range):
+            result.add(sequence[index] + permutation)
+    return result
 
 
 if __name__ == "__main__":
@@ -43,3 +51,18 @@ if __name__ == "__main__":
     print("Expected Output:", ["abc", "acb", "bac", "bca", "cab", "cba"])
     print("Actual Output:", get_permutations("abc"))
     print("--------------------------------------")
+    print("Input:", "ws")
+    print("Expected Output:", ["ws", "sw"])
+    print("Actual Output:", get_permutations("ws"))
+    print("--------------------------------------")
+    print("Input:", "qqe")
+    print("Expected Output:", ["qqe", "qeq", "eqq"])
+    print("Actual Output:", get_permutations("qqe"))
+    print("--------------------------------------")
+    print("Input:", "h")
+    print("Expected Output:", ["h"])
+    print("Actual Output:", get_permutations("h"))
+    print("--------------------------------------")
+    print("Input:", "ddd")
+    print("Expected Output:", ["ddd"])
+    print("Actual Output:", get_permutations("ddd"))
